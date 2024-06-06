23d552d8bda559746da37cc3de5fae9342d2c496
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-06-06 23:35:02 +0300

    feat: done second task

--- models/base_model.py
+++ models/base_model.py
@@ -2,20 +2,30 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updateed_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    pass
+                elif key == "updated_at" or key == "created_at":
+                    print(key, value)
+                    self.__dict__[key] =  datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
     
     def __str__(self):
         return f"[{self.__class__.__name__}] {self.id} {self.__dict__}"
     
     def save(self):
-        self.updateed_at = datetime.now()
+        self.updated_at = datetime.now()
     
     def to_dict(self):
         new_dict = self.__dict__
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updateed_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
