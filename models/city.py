acce32847fd6f739503513f98c36c8a438e9a271
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-06-07 00:54:08 +0300

    feat: done task 9

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines class City
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City Class
+    """
+    state_id = ""
+    name = ""
